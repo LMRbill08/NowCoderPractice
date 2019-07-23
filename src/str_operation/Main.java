621eb3a188b5bcaf34fa52d19189edbeab2a4206
Misakaaa <453951256@qq.com>
2019-07-23 17:36:30 +1000

    update

---
+++ src/str_operation/Main.java
@@ -0,0 +1,31 @@
+package str_operation;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("===============Print The First Not Repeating Char in String===============");
+		String firstSingleOccurChar = "google";
+		System.out.println("String is: " + firstSingleOccurChar);
+		int firstChar = FirstNotRepeatingChar.func2(firstSingleOccurChar);
+		System.out.println("Index of first not repeating char is: " + firstChar);
+		System.out.println();
+
+		System.out.println("===============Left Rotate String===============");
+		String leftRotateStr = "abcdefg";
+		int rotateBit = 3;
+		System.out.println("String is: " + leftRotateStr);
+		System.out.println("Rotate Bit is: " + rotateBit);
+		System.out.println("Result is: " + LeftRotateString.function(leftRotateStr, rotateBit));
+		System.out.println("");
+		
+		System.out.println("==============Reverse Sentence==============");
+		String strRev = "Hi, this is Bill. ";
+		System.out.println(strRev);
+		System.out.println(ReverseSentence.function(strRev));
+		
+		
+	}
+
+}
---
+++ src/str_operation/ReverseSentence.java
@@ -0,0 +1,19 @@
+package str_operation;
+
+public class ReverseSentence {
+	public static String function(String str) {
+		if (str.trim().equals("")) {
+			return str;
+		}
+		String[] arr = str.split(" ");
+		StringBuilder sBuilder = new StringBuilder();
+		for (int i = arr.length - 1; i >=0; i--) {
+			sBuilder.append(arr[i]);
+			if (i > 0) {
+				sBuilder.append(" ");
+			}
+		}
+		return sBuilder.toString();
+		
+	}
+}
