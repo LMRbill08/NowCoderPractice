4103108f53a8ee9e1c58e7481f736415fed1cd3e
Misakaaa <453951256@qq.com>
2019-08-13 15:37:45 +0800

    update

--- src/listnode/EntryNodeOfLoop.java
+++ src/listnode/EntryNodeOfLoop.java
@@ -2,7 +2,24 @@ package listnode;
 
 public class EntryNodeOfLoop {
 	public static ListNode findEntryNode(ListNode head) {
-
-		return null;
+		ListNode fastNode = head;
+		ListNode slowNode = head;
+		while (fastNode != null && fastNode.next != null) {
+			fastNode = fastNode.next.next;
+			slowNode = slowNode.next;
+			if (fastNode == slowNode) {
+				break;
+			}
+		}
+		if (fastNode == null || fastNode.next == null) {
+			System.out.println("链表中无环！");
+			return null;
+		}
+		slowNode = head;
+		while (slowNode != fastNode) {
+			slowNode = slowNode.next;
+			fastNode = fastNode.next;
+		}
+		return slowNode;
 	}
 }
